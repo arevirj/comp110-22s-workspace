4069113e875dee852610ed77066f934742b6c2a9
Justin Rivera <jrivera1@unc.edu>
2022-01-14 21:26:57 -0500

    Added quotes to student ID

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,6 +1,6 @@
-"""My first program for COMP110"""
+"""My first program for COMP110."""
 
-__author__ = 730488173
+__author__ = "730488173"
 
 print("Hello, world.")
 print("Hello, world, I am eren yeager")
